932e324cc129c3b1cce5d59b85865f2e382ae1cb
IdirX69 <adil.idirr@gmail.com>
2023-03-02 15:04:34 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+$nom = "idir";
+
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Idir" => [
+        "city" => "Lyon",
+        "weapon" => "Bow"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ]
+];
+
+var_dump($characters);
+
